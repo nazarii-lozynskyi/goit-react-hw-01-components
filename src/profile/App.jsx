61b8ba39afede1c554cae83c384209e1c03c880a
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-09-24 22:01:29 +0200

    Create App.jsx

---
+++ src/profile/App.jsx
@@ -0,0 +1,18 @@
+import Profile from './components/Profile';
+import './App.css';
+
+import user from './user.json';
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+}
