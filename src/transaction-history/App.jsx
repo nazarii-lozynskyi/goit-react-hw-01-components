fa18e311e01faebbb7d7d8754366ed91ff0e2619
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-09-25 12:37:28 +0200

    Create App.jsx

---
+++ src/transaction-history/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+import TransactionHistoryTable from './components/TransactionHistoryTable';
+
+import transactions from './transactions.json';
+
+export default function App() {
+  return (
+    <div>
+      <TransactionHistoryTable transactions={transactions} />
+    </div>
+  );
+}
