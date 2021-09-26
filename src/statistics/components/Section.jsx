719f9daf96c48c0bb2a20f95ee21a76344cdfc64
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-09-26 14:22:17 +0200

    Update Section.jsx

--- src/statistics/components/Section.jsx
+++ src/statistics/components/Section.jsx
@@ -1,9 +1,11 @@
 import PropTypes from 'prop-types';
 
+import styles from '../Statistics.module.css';
+
 function Section({ title, children }) {
   return (
-    <section className="statistics">
-      {title && <h2 className="title">{title}</h2>}
+    <section className={styles.statistics}>
+      {title && <h2 className={styles.title}>{title}</h2>}
 
       {children}
     </section>
