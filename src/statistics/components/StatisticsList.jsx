a9c208ebf27e41825c1a96074275370882f78617
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-09-26 14:22:26 +0200

    Update StatisticsList.jsx

--- src/statistics/components/StatisticsList.jsx
+++ src/statistics/components/StatisticsList.jsx
@@ -2,12 +2,36 @@ import PropTypes from 'prop-types';
 
 import Statistics from './Statistics';
 
+import styles from '../Statistics.module.css';
+
+const backgroundColor = [
+  {
+    color: '#eeaeca',
+  },
+  {
+    color: '#c2aeee',
+  },
+  {
+    color: '#c14242',
+  },
+  {
+    color: '#4bb43f',
+  },
+  {
+    color: '#e1e908',
+  },
+];
+
 function StatisticsList({ stats }) {
   //const title = false;
   return (
-    <ul>
+    <ul className={styles.list}>
       {stats.map(statisticalData => (
-        <Statistics key={statisticalData.id} stats={statisticalData} />
+        <Statistics
+          key={statisticalData.id}
+          stats={statisticalData}
+          options={backgroundColor}
+        />
       ))}
     </ul>
   );
