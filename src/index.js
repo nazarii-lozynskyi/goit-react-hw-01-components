b3e807247c7b1c3f0229b9a40859fcfb203018a3
Nazarii Lozynskyi <nazarko.l.m@gmail.com>
2021-09-26 14:21:02 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Profile from './profile/App';
-import Statistics from './statistics/App';
-import Friends from './friends/App';
-import TransactionHistory from './transaction-history/App';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Profile />
+    <App />
   </React.StrictMode>,
-  document.getElementById('task-01')
-);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Statistics />
-  </React.StrictMode>,
-  document.getElementById('task-02')
-);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Friends />
-  </React.StrictMode>,
-  document.getElementById('task-03')
-);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <TransactionHistory />
-  </React.StrictMode>,
-  document.getElementById('task-04')
+  document.getElementById('#root')
 );
